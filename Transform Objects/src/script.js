8e19e862ceb7a0889d1bbaaebbd03650431da543
Aryaman Kushwaha <144326567+aryaman-0011@users.noreply.github.com>
2024-12-02 23:33:28 +0530

    Added group

--- Transform Objects/src/script.js
+++ Transform Objects/src/script.js
@@ -9,30 +9,61 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.x = 0.7;
-mesh.position.y = -0.6;
-mesh.position.z = 1;
 
-// mesh.position.set(0.7, -0.6, 1); // Short-hand for positioning mesh in x, y, z axis.
+const group = new THREE.Group()
+group.scale.y = 2
+group.rotation.y = 0.2
+scene.add(group)
 
-scene.add(mesh);
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x000ff })
+)
+cube1.position.x = - 1.5
+group.add(cube1)
 
-// Scale Mesh
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.x = 0
+group.add(cube2)
 
-mesh.scale.x = 2;
-mesh.scale.y = 0.5;
-mesh.scale.z = 0.5;
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube3.position.x = 1.5
+group.add(cube3)
 
-// mesh.scale.set(2, 0.5, 0.5);
 
 
-// Rotation
-mesh.rotation.reorder('YXZ')
-mesh.rotation.y = Math.PI * 0.25
-mesh.rotation.x = Math.PI * 0.25
+
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
+
+// // mesh.position.set(0.7, -0.6, 1); // Short-hand for positioning mesh in x, y, z axis.
+
+// scene.add(mesh);
+
+// // Scale Mesh
+
+// mesh.scale.x = 2;
+// mesh.scale.y = 0.5;
+// mesh.scale.z = 0.5;
+
+// // mesh.scale.set(2, 0.5, 0.5);
+
+
+// // Rotation
+// mesh.rotation.reorder('YXZ')
+// mesh.rotation.y = Math.PI * 0.25
+// mesh.rotation.x = Math.PI * 0.25
 
 
 // Axes Helper
@@ -54,7 +85,7 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
 
-camera.lookAt(mesh.position)  // Look at the mesh
+// camera.lookAt(mesh.position)  // Look at the mesh
 
 /**
  * Renderer
