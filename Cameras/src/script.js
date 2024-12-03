e4e68f2868f32800f258eb150c608dbd2b664969
Aryaman Kushwaha <144326567+aryaman-0011@users.noreply.github.com>
2024-12-03 22:47:15 +0530

    Used OrbitControls and Damping

--- Cameras/src/script.js
+++ Cameras/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 // Cursor
 
@@ -58,6 +59,12 @@ camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.target.y = 1;
+controls.update();
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
@@ -74,10 +81,13 @@ const tick = () => {
   //   mesh.rotation.y = elapsedTime;
 
   // Update camera
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  camera.position.y = cursor.y * 5;
-  camera.lookAt(mesh.position);
+  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.y = cursor.y * 5;
+  //   camera.lookAt(mesh.position);
+
+  // Update Controls
+  controls.update();
 
   // Render
   renderer.render(scene, camera);
