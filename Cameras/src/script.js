d33785d8dc7d9cff9c69f2e9df2b57a6b8ab8a54
Aryaman Kushwaha <144326567+aryaman-0011@users.noreply.github.com>
2024-12-03 22:16:01 +0530

    Added cursor cordinates

--- Cameras/src/script.js
+++ Cameras/src/script.js
@@ -1,5 +1,18 @@
 import * as THREE from "three";
 
+// Cursor
+
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = event.clientY / sizes.height - 0.5;
+  console.log(cursor.x);
+});
+
 /**
  * Base
  */
@@ -23,21 +36,25 @@ const mesh = new THREE.Mesh(
 scene.add(mesh);
 
 // Camera
-// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-
-const aspectRatio = sizes.width / sizes.height;
-
-const camera = new THREE.OrthographicCamera(
-  -1 * aspectRatio,
-  1 * aspectRatio,
-  1,
-  -1,
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
   0.1,
   100
 );
-camera.position.x = 2;
-camera.position.y = 2;
-camera.position.z = 2;
+
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// );
+// camera.position.x = 2;
+// camera.position.y = 2;
+camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
@@ -54,7 +71,7 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  mesh.rotation.y = elapsedTime;
+  //   mesh.rotation.y = elapsedTime;
 
   // Render
   renderer.render(scene, camera);
