bdf1fc02c935e82cddd2c1d434e873607efa9e09
Aryaman Kushwaha <144326567+aryaman-0011@users.noreply.github.com>
2024-12-08 23:59:18 +0530

    Adding checkbox to make cube visible

--- Debug ui/src/script.js
+++ Debug ui/src/script.js
@@ -27,6 +27,7 @@ const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 gui.add(mesh.position, "y").min(-3).max(3).step(0.01).name('elevation');
+gui.add(mesh, 'visible')
 
 /**
  * Sizes
