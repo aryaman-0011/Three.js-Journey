af93d43736e2cd967f96b2ed5b8e84ffffc146a4
Aryaman Kushwaha <144326567+aryaman-0011@users.noreply.github.com>
2024-12-09 00:16:54 +0530

    Fixed color issue

--- Debug ui/src/script.js
+++ Debug ui/src/script.js
@@ -8,6 +8,7 @@ import GUI from "lil-gui";
  */
 
 const gui = new GUI();
+const debugObject = {};
 
 /**
  * Base
@@ -21,15 +22,20 @@ const scene = new THREE.Scene();
 /**
  * Object
  */
+
+debugObject.color = "#a778d8";
+
 const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
-const material = new THREE.MeshBasicMaterial({ color: "#ff0000" });
+const material = new THREE.MeshBasicMaterial({ color: debugObject.color });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 gui.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
 gui.add(mesh, "visible");
 gui.add(material, "wireframe");
-gui.addColor(material, "color");
+gui.addColor(debugObject, "color").onChange((value) => {
+  material.color.set(debugObject.color);
+});
 
 /**
  * Sizes
