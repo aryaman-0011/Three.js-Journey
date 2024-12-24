1c49fa017deb95a6dd38544ad8705ae79d1f98ab
Aryaman Kushwaha <144326567+aryaman-0011@users.noreply.github.com>
2024-12-24 23:40:58 +0530

    alphaMap

--- Materials/src/script.js
+++ Materials/src/script.js
@@ -99,6 +99,8 @@ material.metalnessMap = doorMetalnessTexture;
 material.roughnessMap = doorRoughnessTexture;
 material.normalMap = doorNormalTexture;
 material.normalScale.set(0.5, 0.5);
+material.transparent = true;
+material.alphaMap = doorAlphaTexture;
 
 gui.add(material, "metalness").min(0).max(1).step(0.0001);
 gui.add(material, "roughness").min(0).max(1).step(0.0001);
