640dc4d70fc1b49ca652b3c57d38aba0b40e1071
Aryaman Kushwaha <144326567+aryaman-0011@users.noreply.github.com>
2024-12-27 20:50:32 +0530

    Using IOR with only metalness and roughness

--- Materials/src/script.js
+++ Materials/src/script.js
@@ -107,19 +107,19 @@ matcapTexture.colorSpace = THREE.SRGBColorSpace;
 
 // MeshPhysicalMaterial
 const material = new THREE.MeshPhysicalMaterial();
-material.metalness = 1;
-material.roughness = 1;
-material.map = doorColorTexture;
-material.aoMap = doorAmbientOcclusionTexture;
-material.aoMapIntensity = 1;
-material.displacementMap = doorHeightTexture;
-material.displacementScale = 0.1;
-material.metalnessMap = doorMetalnessTexture;
-material.roughnessMap = doorRoughnessTexture;
-material.normalMap = doorNormalTexture;
-material.normalScale.set(0.5, 0.5);
-material.transparent = true;
-material.alphaMap = doorAlphaTexture;
+material.metalness = 0;
+material.roughness = 0;
+// material.map = doorColorTexture;
+// material.aoMap = doorAmbientOcclusionTexture;
+// material.aoMapIntensity = 1;
+// material.displacementMap = doorHeightTexture;
+// material.displacementScale = 0.1;
+// material.metalnessMap = doorMetalnessTexture;
+// material.roughnessMap = doorRoughnessTexture;
+// material.normalMap = doorNormalTexture;
+// material.normalScale.set(0.5, 0.5);
+// material.transparent = true;
+// material.alphaMap = doorAlphaTexture;
 
 gui.add(material, "metalness").min(0).max(1).step(0.0001);
 gui.add(material, "roughness").min(0).max(1).step(0.0001);
