12015f93de72179d403724af165eeac5c7716fef
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-01-25 23:03:04 +0530

    Easy method of centering the text

--- 3d-text/src/script.js
+++ 3d-text/src/script.js
@@ -45,13 +45,14 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
             bevelSegments: 4,
         }
     )
-    textGeometry.computeBoundingBox()
-    textGeometry.translate(
-       - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
-       - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
-       - (textGeometry.boundingBox.max.z - 0.03) * 0.5
-    )
+    // textGeometry.computeBoundingBox()
+    // textGeometry.translate(
+    //    - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
+    //    - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
+    //    - (textGeometry.boundingBox.max.z - 0.03) * 0.5
+    // )
 
+    textGeometry.center()
 
     const textMaterial = new THREE.MeshBasicMaterial()
     textMaterial.wireframe = true
