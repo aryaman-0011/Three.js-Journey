ec42788f2e25860e52350acc1248c9a14465c635
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-01-29 21:31:44 +0530

    Adding hemisphereLight in scene and adding tweak for it

--- Classic Techniques/lights/src/script.js
+++ Classic Techniques/lights/src/script.js
@@ -24,8 +24,12 @@ const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.9)
 directionalLight.position.set(1, 0.25, 0)
 scene.add(directionalLight)
 
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.9)
+scene.add(hemisphereLight)
+
 gui.add(ambientLight, 'intensity').min(0).max(3).step(0.001)
 gui.add(directionalLight, 'intensity').min(0).max(3).step(0.001)
+gui.add(hemisphereLight, 'intensity').min(0).max(3).step(0.001)
 
 /**
  * Objects
