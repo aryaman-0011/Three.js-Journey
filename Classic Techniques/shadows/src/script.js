a455b5f1d92dae39fe20133395288ebdb7432c4f
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-01-30 17:32:10 +0530

    Camera near/far values

--- Classic Techniques/shadows/src/script.js
+++ Classic Techniques/shadows/src/script.js
@@ -53,6 +53,8 @@ const spotLight = new THREE.SpotLight(0xffffff, 5, 10, Math.PI * 0.3)
 spotLight.castShadow = true
 spotLight.shadow.mapSize.width = 1024
 spotLight.shadow.mapSize.height = 1024
+spotLight.shadow.camera.near = 1
+spotLight.shadow.camera.far = 6
 
 spotLight.position.set(0, 2, 2)
 scene.add(spotLight)
