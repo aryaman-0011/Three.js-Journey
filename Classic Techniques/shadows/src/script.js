b9c4a10197ffa75778e3a24141fdfe50e6d65872
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-01-30 17:02:44 +0530

    Shadow Blurr

--- Classic Techniques/shadows/src/script.js
+++ Classic Techniques/shadows/src/script.js
@@ -41,8 +41,10 @@ directionalLight.shadow.camera.bottom = -2
 directionalLight.shadow.camera.left = -2
 directionalLight.shadow.camera.near = 1
 directionalLight.shadow.camera.far = 6
+directionalLight.shadow.radius = 10
 
 const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+directionalLightCameraHelper.visible = false
 scene.add(directionalLightCameraHelper)
 
 /**
