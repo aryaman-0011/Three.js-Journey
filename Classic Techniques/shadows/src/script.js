2aaf9a02fb58df956134ed2d06929139ca63b386
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-01-31 12:34:14 +0530

    Adjusting value of camera and shadow

--- Classic Techniques/shadows/src/script.js
+++ Classic Techniques/shadows/src/script.js
@@ -66,7 +66,13 @@ scene.add(spotLightCameraHelper)
 
 // Point Light
 const pointLight = new THREE.PointLight(0xffffff, 2.7)
+
 pointLight.castShadow = true
+pointLight.shadow.mapSize.width = 1024
+pointLight.shadow.mapSize.height = 1024
+pointLight.shadow.camera.near = 0.1
+pointLight.shadow.camera.far = 5
+
 pointLight.position.set(-1, 1, 0)
 scene.add(pointLight)
 
