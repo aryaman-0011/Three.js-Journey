798b4acefb4480e506985d40d433483a06e500cf
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-02-04 15:06:18 +0530

    Using depthWrite to fix particle edges

--- Classic Techniques/particles/src/script.js
+++ Classic Techniques/particles/src/script.js
@@ -48,7 +48,8 @@ particlesMaterial.color = new THREE.Color('#ff88cc')
 particlesMaterial.transparent = true
 particlesMaterial.alphaMap = particleTexture
 // particlesMaterial.alphaTest = 0.001
-particlesMaterial.depthTest = false
+// particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false
 
 // Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
