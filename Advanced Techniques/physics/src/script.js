9b7b89f47796c8184f041ff2aa9b438b2d09f7ea
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-02-22 10:57:38 +0530

    Added sphere in physics world

--- Advanced Techniques/physics/src/script.js
+++ Advanced Techniques/physics/src/script.js
@@ -39,6 +39,16 @@ const environmentMapTexture = cubeTextureLoader.load([
 const world = new CANNON.World()
 world.gravity.set(0, -9.82, 0)
 
+// Sphere
+const sphereShape = new CANNON.Sphere(0.5)
+const sphereBody = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape: sphereShape
+})
+
+world.addBody(sphereBody)
+
 /**
  * Test sphere
  */
