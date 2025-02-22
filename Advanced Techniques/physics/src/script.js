901518c519811f77ea90346b590404d3c93e8057
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-02-22 11:03:37 +0530

    Update physics world

--- Advanced Techniques/physics/src/script.js
+++ Advanced Techniques/physics/src/script.js
@@ -148,9 +148,15 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+let oldElapsedTime = 0
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - oldElapsedTime
+    oldElapsedTime = elapsedTime
+
+    // Update Physics World
+    world.step(1 / 60, deltaTime, 3)
 
     // Update controls
     controls.update()
